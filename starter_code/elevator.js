6228015233beba48b5b6d8e747e397fdb5fe887d
nachobaile <nachobaile@gmail.com>
2018-01-30 09:31:25 +0100

    casi se borra

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -3,17 +3,54 @@ class Elevator {
     this.floor      = 0;
     this.MAXFLOOR   = 10;
     this.requests   = [];
+    this.direction = 'up';
+    
   }
 
-  start() { }
-  stop() { }
-  update() { }
+  start() {
+    let interval = setInterval(() =>{
+    //Compara la planta con la maxima posible.
+    if(this.floor < this.MAXFLOOR && this.direction=="up"){
+      this.floorUp();
+      this.update();
+    }else{
+      this.floorDown();
+      this.update();
+    }
+  },1000)
+  }
+  stop() { 
+    clearInterval(this.interval);
+  }
+  update() { 
+    this.log();
+
+  }
   _passengersEnter() { }
   _passengersLeave() { }
-  floorUp() { }
-  floorDown() { }
-  call() { }
-  log() { }
+  floorUp() {
+    if(this.floor < this.MAXFLOOR){
+      this.floor++;
+      this.direction = "up";
+    } else {
+      this.direction = "down";
+    }
+   }
+  floorDown() {
+    if(this.floor>0){
+      this.floor--;
+      this.direction ="down";
+    }else{
+      this.direction= "up";
+    }
+  }
+  call() { 
+    
+  }
+  log() {
+    console.log(`direction : ${this.direction}`);
+    console.log(`floor : ${this.floor}`);
+   }
 }
 
 module.exports = Elevator;
--- starter_code/index.js
+++ starter_code/index.js
@@ -1,1 +1,19 @@
 const Elevator = require('./elevator.js');
+const Person = require('./person.js');
+
+const elevator1= new Elevator();
+const Epi = new Person('epi', 1 , 4);
+const Blas = new Person('Blas', 8 , 1);
+const Coco = new Person('Coco', 3 , 6);
+const Espinete= new Person('Espinete', 5 , 3);
+const Tricky= new Person('Tricky', 7 , 2);
+const Chema= new Person('Chema', 10 , 4);
+
+elevator1.call(epi);
+elevator1.call(Blas);
+elevator1.call(Coco);
+elevator1.call(Espinete);
+elevator1.call(tricky);
+elevator1.call(Chema);
+
+elevator1.start();
